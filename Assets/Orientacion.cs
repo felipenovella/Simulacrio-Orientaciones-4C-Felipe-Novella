f40d797e6347a08a05910bea5a2c971395452e1d
felipenovella <49439231@est.ort.edu.ar>
2025-04-15 08:59:25 -0300

    ejerciciocompletadopicante

---
+++ Assets/Orientacion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orientacion : MonoBehaviour
+{ 
+
+
+    public string nombreAlumno;
+    public int añoAlumno;
+    public string orientacionAlumno;
+
+
+
+
+    
+    void Start()
+    {
+        if (nombreAlumno == "")
+
+        {
+            Debug.Log("Falta nombre");
+            return;
+
+        } if (añoAlumno > 0 && añoAlumno < 3)
+        {
+            Debug.Log("Aun estás en el ciclo basico");
+            return;
+        } else if (añoAlumno < 0 || añoAlumno > 5)
+        {
+            Debug.Log("Año incorrecto");
+            return;
+        }
+        
+        if (orientacionAlumno == "T" || orientacionAlumno == "D" || orientacionAlumno == "G" || orientacionAlumno == "M" || orientacionAlumno == "H")
+        {
+            Debug.Log("Muchas gracias " + nombreAlumno);
+            
+
+        } else
+        {
+            Debug.Log("Orientacion Incorrecta");
+        }
+
+
+    } 
+       
+
+    
+    void Update()
+    {
+        
+    }
+}
